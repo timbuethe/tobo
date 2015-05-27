80c956d35708e430ffda6fd246f1e2e7ab054831
Chris <schwarz.c@gmail.com>
2015-05-27 23:02:12 +0200

    Meine ersten Versuche

--- src/chris/Cobo.java
+++ src/chris/Cobo.java
@@ -9,18 +9,45 @@ public class Cobo extends Robot {
 
     public void run() {
         //noinspection InfiniteLoopStatement
-
+        while (true) {
         //Separate the rader movement from the gun's.
         setAdjustRadarForGunTurn(true);
         //Scan for the enemy
         turnRadarRight(360);
     }
+    }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        double bearing = e.getBearing();
-        turnRight(bearing);
 
-        fire(1);
+        double distance = 0;
+
+        turnRight(e.getBearing());
+        distance = e.getDistance();
+
+        if(distance<200)
+        {
+            fire(3.5);
+        }
+        else if(distance<500)
+        {
+            fire(2.5);
+        }
+        else if(distance<800)
+        {
+            fire(1.5);
+        }
+        else
+        {
+            fire(0.5);
+        }
+    }
+
+    //falls getroffen
+    public void onHitByBullet(HitByBulletEvent e) {
+        // Replace the next line with any behavior you would like
+
+        back(20);
+        fire(5);
     }
 /**
     //
